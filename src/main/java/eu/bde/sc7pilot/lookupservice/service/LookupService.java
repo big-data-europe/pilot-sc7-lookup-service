8d09bb5dad9ec1123bd6c47e0aafc1d90b94a24f
gpapadis <gpapadis@yahoo.gr>
2017-01-16 10:46:41 +0200

    Update LookupService.java

--- src/main/java/eu/bde/sc7pilot/lookupservice/service/LookupService.java
+++ src/main/java/eu/bde/sc7pilot/lookupservice/service/LookupService.java
@@ -18,6 +18,21 @@ import eu.bde.sc7pilot.lookupservice.webconfig.ResponseMessage;
 @Path("/")
 public class LookupService {
 
+    private final static String filePath = "/gadm28.csv";
+    private FuzzySearch fs;
+    
+    public LookupService() {
+        try {
+            fs = new FuzzySearch(filePath);
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            respMessage.setMessage(e.getMessage());
+            respMessage.setCode(400);
+            throw new WebApplicationException(
+                    Response.status(HttpURLConnection.HTTP_BAD_REQUEST).entity(respMessage).build());
+        }
+    }
+    
     @POST
     @Path("/geocode")
     @Produces(MediaType.APPLICATION_JSON)
@@ -26,19 +41,6 @@ public class LookupService {
         ResponseMessage respMessage = new ResponseMessage();
         if (locationNames == null || locationNames.isEmpty()) {
             respMessage.setMessage("a non empty list of location names is required into the request body.");
-            respMessage.setCode(400);
-            throw new WebApplicationException(
-                    Response.status(HttpURLConnection.HTTP_BAD_REQUEST).entity(respMessage).build());
-        }
-
-        String filePath = "/gadm28.csv";
-
-        FuzzySearch fs = null;
-        try {
-            fs = new FuzzySearch(filePath);
-        } catch (Exception e) {
-            // TODO Auto-generated catch block
-            respMessage.setMessage(e.getMessage());
             respMessage.setCode(400);
             throw new WebApplicationException(
                     Response.status(HttpURLConnection.HTTP_BAD_REQUEST).entity(respMessage).build());
